c3948e7341035dfa91b3a53336d074921c643bcc
Chris Balow Jr <cmbalow@illuminateed.net>
2019-01-22 14:29:02 -0500

    refactored to remove classes

--- app.js
+++ app.js
@@ -1,50 +1,46 @@
 // DOM VARIABLES
 const gameBoxContainer = document.querySelector('.game-box-container');
 const gameBoxes = document.querySelectorAll('.game-box');
-const gamePiece = ['X', 'O'];
+const gamePiece = ['ex','oh'];
+const newGame = document.querySelector('.new-game');
+let usedPiece;
+let game;
 
-// CLASSES
-class Game { 
-
-  constructor() { }
-
-  static addMark(e) {
-    console.log(gamePiece);
+function addMark(e) {
     if (!e.target.classList.contains('game-box')) {
       return;
     }
     if (!e.target.classList.contains('selected')) {
       let box = e.target;
       box.classList.add('selected');
-      box.innerHTML = gamePiece[0];
-      let usedPiece = gamePiece.shift();
+      box.classList.add(gamePiece[0]);
+      usedPiece = gamePiece.shift();
       gamePiece.push(usedPiece);
     } 
   }
 
-  switchTurn() { }
-  
-  gameOver() { }
-
-}
-
-class Player { 
-
-  constructor() { }
-
-}
-
-class GameBoard { 
-
-  constructor() { 
-    const gameArray = {
-      rowOne  : ['box-one', 'box-two', 'box-three'],
-      rowTwo  : ['box-four', 'box-five', 'box-six'],
-      rowThree: ['box-seven', 'box-eight', 'box-nine']
-    };
-  }
-
-}
-
 // EVENT LISTENERS
-gameBoxContainer.addEventListener('click', Game.addMark);
+gameBoxContainer.addEventListener('click', addMark);
+
+gameBoxes.forEach((box) => {
+  box.addEventListener('click', (e) => {
+    console.log(e.target);
+  });
+});
+
+// newGame.addEventListener('click', (e) => {
+//   game = {
+//     box_one: 'x', box_two: 'x', box_three: 'x',
+//     box_four: 'x', box_five: 'x', box_six: 'x',
+//     box_seven: 'x', box_eight: 'x', box_nine: 'x',
+//   };
+//   // console.log(game);
+//   gameBoxes.forEach((box) => {
+//     Object.keys(game).forEach((key) => {
+//       if (key == box.classList[1]) {
+//         box.innerHTML = game.box_eight;
+//       }
+//     });
+//   });
+//   e.preventDefault();
+// });
