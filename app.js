5fb2d827d2960460108622fc882e19b700fdc2e4
Chris Balow Jr <cmbalow@illuminateed.net>
2019-01-24 20:31:55 -0500

    basic logic of finding gameWinner implemented

--- app.js
+++ app.js
@@ -1,46 +1,75 @@
 // DOM VARIABLES
 const gameBoxContainer = document.querySelector('.game-box-container');
 const gameBoxes = document.querySelectorAll('.game-box');
+const declareWinner = document.querySelector('.declare-winner');
 const gamePiece = ['ex','oh'];
-const newGame = document.querySelector('.new-game');
 let usedPiece;
-let game;
 
+const winningPatterns = ["123","147","159","258","357","369","456","789"];
+
+let patternsX = [];
+let patternsO = [];
+
+// FUNCTIONS
+// TODO:
+// // every time addMark runs, we need to 
+// // add element either to player 1 or player 2 array
+// // check both arrays for a match with winningPatterns
+// // endgame and declare winning if found, do nothing if not
 function addMark(e) {
-    if (!e.target.classList.contains('game-box')) {
-      return;
-    }
-    if (!e.target.classList.contains('selected')) {
-      let box = e.target;
-      box.classList.add('selected');
-      box.classList.add(gamePiece[0]);
-      usedPiece = gamePiece.shift();
-      gamePiece.push(usedPiece);
-    } 
+  if (!e.target.classList.contains('game-box')) {
+    return;
   }
+  if (!e.target.classList.contains('selected')) {
+    let box = e.target;
+    box.classList.add('selected');
+    box.classList.add(gamePiece[0]);
+    usedPiece = gamePiece.shift();
+    gamePiece.push(usedPiece);
+    if (e.target.classList.contains('ex')) {
+      patternsX.push(parseInt(e.target.classList[1]));
+      patternsX.sort();
+      let xCombos = getCombinations(patternsX);
+      findMatch(xCombos, winningPatterns);
+    } else {
+      patternsO.push(parseInt(e.target.classList[1]));
+      patternsO.sort();
+      let oCombos = getCombinations(patternsO);
+      findMatch(oCombos, winningPatterns);
+    }
+  } 
+}
+
+// get every possible 3-digit combination of every element in array
+function getCombinations(chars) {
+  var result = [];
+  var f = function(prefix, chars) {
+    for (var i = 0; i < chars.length; i++) {
+      result.push(prefix + chars[i]);
+      f(prefix + chars[i], chars.slice(i + 1));
+    }
+  }
+  f('', chars);
+  filteredResult = result.filter(e => e.length === 3);
+  return filteredResult;
+}
+
+// Check for winniner by finding out if 
+// one array contains a value found in another array
+function findMatch(aryOne, aryTwo) {
+  let matches = [];
+  for (i = 0; i < aryOne.length; i++) {
+    for (j = 0; j < aryTwo.length; j++) {
+      if (aryOne[i] == aryTwo[j]) {
+        // matches.push(aryOne[i]);
+        let winnerText = document.createElement('h2');
+        winnerText.textContent = 'We have a winner!';
+        declareWinner.insertBefore(winnerText, null);
+      }
+    }
+  }
+  // return matches;
+}
 
 // EVENT LISTENERS
 gameBoxContainer.addEventListener('click', addMark);
-
-gameBoxes.forEach((box) => {
-  box.addEventListener('click', (e) => {
-    console.log(e.target);
-  });
-});
-
-// newGame.addEventListener('click', (e) => {
-//   game = {
-//     box_one: 'x', box_two: 'x', box_three: 'x',
-//     box_four: 'x', box_five: 'x', box_six: 'x',
-//     box_seven: 'x', box_eight: 'x', box_nine: 'x',
-//   };
-//   // console.log(game);
-//   gameBoxes.forEach((box) => {
-//     Object.keys(game).forEach((key) => {
-//       if (key == box.classList[1]) {
-//         box.innerHTML = game.box_eight;
-//       }
-//     });
-//   });
-//   e.preventDefault();
-// });
